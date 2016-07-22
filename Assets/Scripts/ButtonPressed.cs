95e231754e0eac32e4e98819f789baa47f4e9dd2
jaden <jadenjgregory5476@gmail.com>
2016-07-22 17:49:20 +1000

    GUI Framework Changed
    Basic GUI manager working

---
+++ Assets/Scripts/ButtonPressed.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ButtonPressed : MonoBehaviour {
+
+    public GuiManager Manager;
+
+    public void ViewHud()
+    {
+        Manager.ToggleActiveByID(0, true);
+    }
+}
--- Assets/Scripts/GuiManager.cs
+++ Assets/Scripts/GuiManager.cs
@@ -1,15 +1,55 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class GuiManager : MonoBehaviour {
+public class GuiManager : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
-	void Update () {
-	
-	}
+    public GuiTab[] Tabs;
+
+
+
+    public void ToggleActiveByName(string _name, bool state)
+    {
+        for (int i = 0; i < Tabs.Length; i++)
+        {
+            if (Tabs[i].name == _name)
+            {
+                Tabs[i].GuiItems.SetActive(state);
+                break;
+            }
+            
+        }
+    }
+
+    public void ToggleActiveByID(int _ID, bool state)
+    {
+        for (int i = 0; i < Tabs.Length; i++)
+        {
+            if (Tabs[i].ID == _ID)
+            {
+                Tabs[i].GuiItems.SetActive(state);
+                break;
+            }
+        }
+    }
+
+    void Awake()
+    {
+        for (int i = 0; i < Tabs.Length; i++)
+        {
+            Tabs[i].GuiItems.gameObject.SetActive(Tabs[i].DefaultState);
+        }
+    }
+
+
+    [System.Serializable]
+    public struct GuiTab
+    {
+
+        public GameObject GuiItems;
+        public string name;
+        public int ID;
+        public bool DefaultState;
+    }
+
 }
